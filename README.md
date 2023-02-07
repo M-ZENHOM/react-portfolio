5a5e7768362716421d533fa2348730eb9dd964c6
ZENHOM <you@example.com>
2023-02-07 02:45:09 +0200

    upload projects

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 ﻿# - ZENHOM PORTFOLIO
+
 Hi, this is v2 of my portfolio with react check it out!
+
 # - Demo Link:
 
-https://www.mahmoud-zenhom.cf/
+<a href="https://www.mahmoud-zenhom.cf/">Portfolio Live</a>
 
 # - Some Imgs
 
 <a href='https://postimg.cc/fSjjtBp9' target='_blank'><img src='https://i.postimg.cc/L5GyWWVN/msedge-VFQv4dk-Wn-U.png' border='0' alt='msedge-VFQv4dk-Wn-U'/></a>
 <a href='https://postimg.cc/WDTDtsjX' target='_blank'><img src='https://i.postimg.cc/wBsDfBr8/msedge-f-Q0-N91aq-XL.png' border='0' alt='msedge-f-Q0-N91aq-XL'/></a>
 <a href='https://postimg.cc/KK0YzL08' target='_blank'><img src='https://i.postimg.cc/pTH5sJ8z/msedge-qu-T1w-Pq-Zp-L.png' border='0' alt='msedge-qu-T1w-Pq-Zp-L'/></a>
-
-
-
-
-
--- src/SubComponents/Data.js
+++ src/SubComponents/Data.js
@@ -1,40 +1,23 @@
 import TodoImg from "../assets/images/ProjectsImgs/TodoList.png";
+import CommerceImg from "../assets/images/ProjectsImgs/CommerceApp.png";
 
 export const ProData = [
   {
     id: 1,
-    name: "To-Do List App",
-    tech: "React JS / Styled-Components / Redux/Toolkit /React-Bootstrap",
-    description: "A to-do list app",
-    img: TodoImg,
-    code: "#",
-    demo: "#",
+    name: "Commerce App",
+    tech: "React JS - Styled-Components - Redux/Toolkit - Bootstrap",
+    description: "A simple commerce app with cart and check out with fake api",
+    img: CommerceImg,
+    code: "https://github.com/M-ZENHOM/Simple-Commerce-App-",
+    demo: "https://simple-commerce-app.vercel.app/",
   },
   {
     id: 2,
-    name: "Weather App 2",
-    tech: "React JS / Styled-Components",
-    description: "This is a weather app with a search bar",
+    name: "Todo Lists App",
+    tech: "React JS - Redux/Toolkit - Styled-Components",
+    description: "A smiple todo lists app  with redux/toolkit",
     img: TodoImg,
-    code: "#",
-    demo: "#",
-  },
-  {
-    id: 3,
-    name: "Weather App 3",
-    tech: "React JS / Styled-Components",
-    description: "This is a weather app with a search bar",
-    img: TodoImg,
-    code: "#",
-    demo: "#",
-  },
-  {
-    id: 4,
-    name: "Weather App 4",
-    tech: "React JS / Styled-Components",
-    description: "This is a weather app with a search bar",
-    img: TodoImg,
-    code: "#",
-    demo: "#",
+    code: "https://github.com/M-ZENHOM/To-Do-Lists-with-redux-toolkit",
+    demo: "https://to-do-lists-with-redux-toolkit.vercel.app/",
   },
 ];
