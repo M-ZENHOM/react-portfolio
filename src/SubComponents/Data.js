fbda02f1d60ca175c1d3d7739a12de73fc61b30b
ZENHOM <you@example.com>
2023-02-15 16:13:33 +0200

    Add movie app project

--- src/SubComponents/Data.js
+++ src/SubComponents/Data.js
@@ -1,9 +1,20 @@
 import TodoImg from "../assets/images/ProjectsImgs/TodoList.png";
 import CommerceImg from "../assets/images/ProjectsImgs/CommerceApp.png";
+import MovieImg from "../assets/images/ProjectsImgs/MovieImg.png";
 
 export const ProData = [
   {
     id: 1,
+    name: "Moive App",
+    tech: "React JS - Redux/Toolkit - Mui",
+    description:
+      "A Simple Movie Application using React JS, Styled-component ,allows the user to search and filter movies they want from the MovieDB API",
+    img: MovieImg,
+    code: "https://github.com/M-ZENHOM/REACT_MOIVE_APP",
+    demo: "https://react-moive-app-delta.vercel.app/",
+  },
+  {
+    id: 2,
     name: "Commerce App",
     tech: "React JS - Styled-Components - Redux/Toolkit - Bootstrap",
     description: "A simple commerce app with cart and check out with fake api",
@@ -12,7 +23,7 @@ export const ProData = [
     demo: "https://simple-commerce-app.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     name: "Todo Lists App",
     tech: "React JS - Redux/Toolkit - Styled-Components",
     description: "A smiple todo lists app  with redux/toolkit",
