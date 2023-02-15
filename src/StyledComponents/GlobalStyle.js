a002d2966c865584959420424e9b629fc7865b37
ZENHOM <you@example.com>
2023-02-15 16:24:30 +0200

    style update

--- src/StyledComponents/GlobalStyle.js
+++ src/StyledComponents/GlobalStyle.js
@@ -15,9 +15,9 @@ body {
   background-color: ${({ theme }) => theme.colors.Main};
   font-family: ${({ theme }) => theme.fonts.MainFont};
   overflow: hidden;
-  @media screen and (max-width:768px) {
+  /* @media screen and (max-width:768px) {
     overflow: scroll;
-  }
+  } */
 }
 
 button,
